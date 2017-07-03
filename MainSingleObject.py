de50a5f9e792aacab311661a624a3aad3e159b2b
respatte <a.capelier-mourguy@lancaster.ac.uk>
2017-07-03 17:04:59 +0100

    Shutting warnings down, saving results into subfolder

--- MainSingleObject.py
+++ MainSingleObject.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import time
+import warnings
 from Experiments import SingleObjectExperiment
 
 def main():
+	warnings.filterwarnings("ignore")
 	explo_ratios = [.25,.375,.5,.625,.75]
 	for explo_ratio in explo_ratios:
 		t = time.time()
@@ -11,8 +13,9 @@ def main():
 		print("Starting runs for explo_ratio =", explo_ratio)
 		condition = SingleObjectExperiment((1,10,8),(.1,explo_ratio),24)
 		results = condition.run_experiment()
-		condition.output_data(results,
-							  "SingleObjectExperiment_explo" + str(explo_ratio))
+		condition.output_data(results[0],
+							  "Results/SingleObjectExperiment_explo" +\
+							  str(explo_ratio))
 		t = time.gmtime(time.time() - t)
 		print("Condition finished in", time.strftime("%H:%M:%S",t))
 
