5daaa4d415f32dc9e778108014ea4a6a4ab392cf
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-07-04 14:48:27 +0100

    Put calling to Experiment into a separate function for further multiprocessing

--- MainSingleObject.py
+++ MainSingleObject.py
@@ -2,24 +2,38 @@
 # -*- coding: utf-8 -*-
 import time
 import warnings
+from multiprocessing import Process, Queue
+
 from Experiments import SingleObjectExperiment
+
+def run_subjects(n_subjects, explo_ratio, bash_i, q=None):
+	t = time.time()
+	print("=" * 40)
+	print("Starting runs for explo_ratio =", explo_ratio)
+	condition = SingleObjectExperiment((1,10,8), (.1,explo_ratio),
+									   n_subjects, n_subjects*bash_i)
+	results = condition.run_experiment()
+	t = time.gmtime(time.time() - t)
+	print("Condition finished in", time.strftime("%H:%M:%S",t))
+	if q:
+		q.put(results)
+	return results
 
 def main():
 	warnings.filterwarnings("ignore")
 	explo_ratios = [.25,.375,.5,.625,.75]
+	t_results = []
+	f_results = {}
 	for i, explo_ratio in enumerate(explo_ratios):
-		t = time.time()
-		print("=" * 40)
-		print("Starting runs for explo_ratio =", explo_ratio)
-		condition = SingleObjectExperiment((1,10,8),(.1,explo_ratio),24,
-										   24*i)
-		results = condition.run_experiment()
-		condition.output_data(results[0],
-							  "Results/SingleObjectExperiment_explo" +\
-							  str(explo_ratio))
-		t = time.gmtime(time.time() - t)
-		print("Condition finished in", time.strftime("%H:%M:%S",t))
-	return results
+		results = run_subjects(24, explo_ratio, i)
+		t_results.append(results[1])
+		f_results.update(results[0])
+	SingleObjectExperiment.output_data(f_results,"Results/SingleObject")
+
+def main_multiproc():
+	warnings.filterwarnings("ignore")
+	explo_ratios = [.25,.375,.5,.625,.75]
+	results = Queue()
 
 
 if __name__ == "__main__":
