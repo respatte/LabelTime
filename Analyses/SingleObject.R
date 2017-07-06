7075936d34868e61c50f2f54d904da8989b4baa7
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-07-06 12:21:17 +0100

    Mixed-effect models fitted, compared, best model selected. Graphs to do.

--- Analyses/SingleObject.R
+++ Analyses/SingleObject.R
@@ -2,31 +2,83 @@
 library(ggplot2)
 library(dplyr)
 library(lme4)
+library(sjPlot)
+library(languageR)
 
 # DATA HANDLING
 # Import data
-label_time.data <- read.csv("SingleObject_LT.csv", head=TRUE)
+label_time.data <- read.csv("../Results/SingleObject_LT.csv", head=TRUE)
 # Set all factor variables to factors
-label_time.data$trial <- as.factor(looking_times$trial)
-label_time.data$subject <- as.factor(looking_times$subject)
-label_time.data$explo_overlap <- as.factor(looking_times$explo_overlap)
+# label_time.data$trial <- as.factor(label_time.data$trial)
+label_time.data$subject <- as.factor(label_time.data$subject)
+label_time.data$explo_overlap <- as.factor(label_time.data$explo_overlap)
 label_time.LaF.data <- filter(label_time.data, model == "LaF")
 label_time.CR.data <- filter(label_time.data, model == "CR")
 
 # MODELS
-label_time.lmer1 <- lmer(label_time.data ~ trial + model + labelled +
-						 trial*model + trial*labelled + model*labelled +
-						 trial*model*labelled +
+# Global model for both LaF and CR together
+label_time.lmer.all <- lmer(looking_time ~ trial*model*labelled +
 						 (1 + trial + labelled | subject) +
-						 (1 + trial + labelled + model | explo_ratio)
+						 (1 + trial + labelled + model | explo_overlap),
+						 data = label_time.data
 						 )
-label_time.LaF.lmer1 <- lmer(label_time.LaF.data ~ trial + labelled +
-							 trial*labelled +
-							 (1 + trial + labelled | subject) +
-							 (1 + trial + labelled | explo_ratio)
-							 )
-label_time.CR.lmer1 <- lmer(label_time.CR.data ~ trial + labelled +
-							trial*labelled +
-							(1 + trial + labelled | subject) +
-							(1 + trial + labelled | explo_ratio)
-							)
+# Models for LaF only -- There's no point running models without main effect of trial
+# Including only main effect for trial
+label_time.LaF.trial_only <- lmer(looking_time ~ trial +
+								  (1 + trial + labelled | subject) +
+								  (1 + trial + labelled | explo_overlap),
+								  data = label_time.LaF.data
+								  )
+# Including main effects for trial and labelled
+label_time.LaF.labelled <- lmer(looking_time ~ trial + labelled +
+								(1 + trial + labelled | subject) +
+								(1 + trial + labelled | explo_overlap),
+								data = label_time.LaF.data
+								)
+# Including all interactions and main effects
+label_time.LaF.all <- lmer(looking_time ~ trial*labelled +
+						   (1 + trial + labelled | subject) +
+						   (1 + trial + labelled | explo_overlap),
+						   data = label_time.LaF.data
+						   )
+# Comparing models with different fixed effects
+label_time.LaF.comparison <- anova(label_time.LaF.trial_only,
+								   label_time.LaF.labelled,
+								   label_time.LaF.all)
+# Results: adding all interactions helps the model, not label only
+# Best model: label_time.LaF.all
+# Models for CR only
+# Including only main effect for trial
+label_time.CR.trial_only <- lmer(looking_time ~ trial +
+								 (1 + trial + labelled | subject) +
+								 (1 + trial + labelled | explo_overlap),
+								 data = label_time.CR.data
+								 )
+# Including main effects for trial and labelled
+label_time.CR.labelled <- lmer(looking_time ~ trial + labelled +
+							   (1 + trial + labelled | subject) +
+							   (1 + trial + labelled | explo_overlap),
+							   data = label_time.CR.data
+							   )
+# Including all interactions and main effects
+label_time.CR.all <- lmer(looking_time ~ trial*labelled +
+						  (1 + trial + labelled | subject) +
+						  (1 + trial + labelled | explo_overlap),
+						  data = label_time.CR.data
+						  )
+# Comparing models with different fixed effects
+label_time.CR.comparison <- anova(label_time.CR.trial_only,
+								  label_time.CR.labelled,
+								  label_time.CR.all)
+# Results: adding more parameters than trial alone didn't help the model
+# Best model: label_time.CR.trial_only
+
+# GENERATING GRAPHS
+# Model plot for LaF
+pdf("../Results/LaF_results_LT.pdf", w=8, h=5)
+# TODO
+dev.off()
+# Model plot for CR
+pdf("../Results/CR_results_LT.pdf", w=8, h=5)
+# TODO
+dev.off()
