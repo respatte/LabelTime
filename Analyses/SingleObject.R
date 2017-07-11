f2c26074c0cb2b25595a264c43ba5cf07ff388cd
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-07-11 15:42:38 +0100

    Minor modifications in variable names and graph creation

--- Analyses/SingleObject.R
+++ Analyses/SingleObject.R
@@ -5,88 +5,91 @@ library(lme4)
 
 # DATA HANDLING
 # Import data
-label_time.data <- read.csv("../Results/SingleObject_LT.csv", head=TRUE)
+single_obj.data <- read.csv("../Results/SingleObject_LT.csv", head=TRUE)
 # Set all factor variables to factors
-label_time.data$subject <- as.factor(label_time.data$subject)
-label_time.data$explo_overlap <- as.factor(label_time.data$explo_overlap)
-label_time.LaF.data <- filter(label_time.data, model == "LaF")
-label_time.CR.data <- filter(label_time.data, model == "CR")
+single_obj.data$subject <- as.factor(single_obj.data$subject)
+single_obj.data$explo_overlap <- as.factor(single_obj.data$explo_overlap)
+single_obj.LaF.data <- filter(single_obj.data, model == "LaF")
+single_obj.CR.data <- filter(single_obj.data, model == "CR")
 
 # MODELS
 # Global model for both LaF and CR together
-label_time.lmer.all <- lmer(looking_time ~ trial*model*labelled +
+single_obj.lmer.all <- lmer(looking_time ~ trial*model*labelled +
 							(1 + trial + labelled | subject) +
 							(1 + trial + labelled + model | explo_overlap),
-							data = label_time.data
+							data = single_obj.data
 							)
 # Models for LaF only -- There's no point running models without main effect of trial
 # Including only main effect for trial
-label_time.LaF.lmer.trial_only <- lmer(looking_time ~ trial +
+single_obj.LaF.lmer.trial_only <- lmer(looking_time ~ trial +
 									   (1 + trial + labelled | subject) +
 									   (1 + trial + labelled | explo_overlap),
-									   data = label_time.LaF.data
+									   data = single_obj.LaF.data
 									   )
 # Including main effects for trial and labelled
-label_time.LaF.lmer.labelled <- lmer(looking_time ~ trial + labelled +
+single_obj.LaF.lmer.labelled <- lmer(looking_time ~ trial + labelled +
 									 (1 + trial + labelled | subject) +
 									 (1 + trial + labelled | explo_overlap),
-									 data = label_time.LaF.data
+									 data = single_obj.LaF.data
 									 )
 # Including all interactions and main effects
-label_time.LaF.lmer.all <- lmer(looking_time ~ trial*labelled +
+single_obj.LaF.lmer.all <- lmer(looking_time ~ trial*labelled +
 								(1 + trial + labelled | subject) +
 								(1 + trial + labelled | explo_overlap),
-								data = label_time.LaF.data
+								data = single_obj.LaF.data
 								)
 # Comparing models with different fixed effects
-label_time.LaF.comparison <- anova(label_time.LaF.lmer.trial_only,
-								   label_time.LaF.lmer.labelled,
-								   label_time.LaF.lmer.all)
+single_obj.LaF.comparison <- anova(single_obj.LaF.lmer.trial_only,
+								   single_obj.LaF.lmer.labelled,
+								   single_obj.LaF.lmer.all)
 # Results: adding all interactions helps the model, not label only
-# Best model: label_time.LaF.lmer.all
+# Best model: single_obj.LaF.lmer.all
 # Models for CR only
 # Including only main effect for trial
-label_time.CR.lmer.trial_only <- lmer(looking_time ~ trial +
+single_obj.CR.lmer.trial_only <- lmer(looking_time ~ trial +
 									  (1 + trial + labelled | subject) +
 									  (1 + trial + labelled | explo_overlap),
-									  data = label_time.CR.data
+									  data = single_obj.CR.data
 									  )
 # Including main effects for trial and labelled
-label_time.CR.lmer.labelled <- lmer(looking_time ~ trial + labelled +
+single_obj.CR.lmer.labelled <- lmer(looking_time ~ trial + labelled +
 									(1 + trial + labelled | subject) +
 									(1 + trial + labelled | explo_overlap),
-									data = label_time.CR.data
+									data = single_obj.CR.data
 									)
 # Including all interactions and main effects
-label_time.CR.lmer.all <- lmer(looking_time ~ trial*labelled +
+single_obj.CR.lmer.all <- lmer(looking_time ~ trial*labelled +
 							   (1 + trial + labelled | subject) +
 							   (1 + trial + labelled | explo_overlap),
-							   data = label_time.CR.data
+							   data = single_obj.CR.data
 							   )
 # Comparing models with different fixed effects
-label_time.CR.comparison <- anova(label_time.CR.lmer.trial_only,
-								  label_time.CR.lmer.labelled,
-								  label_time.CR.lmer.all)
+single_obj.CR.comparison <- anova(single_obj.CR.lmer.trial_only,
+								  single_obj.CR.lmer.labelled,
+								  single_obj.CR.lmer.all)
 # Results: adding more parameters than trial alone didn't help the model
-# Best model: label_time.CR.lmer.trial_only
+# Best model: single_obj.CR.lmer.trial_only
 
 # GENERATING GRAPHS
 # Transform trial number to start at 1
-label_time.data$trial <- label_time.data$trial + 1
+single_obj.data$trial <- single_obj.data$trial + 1
 # Graph from data (not models)
-pdf("../Results/General.pdf", w=9, h=4)
-label_time.data.plot <- ggplot(label_time.data) +
+#pdf("../Results/General.pdf", w=9, h=4)
+single_obj.data.plot <- ggplot(single_obj.data) +
 						geom_smooth(size = .2,
 									mapping = aes(trial, looking_time,
-												  colour = labelled)) +
+												  colour = labelled),
+									method = "loess") +
 						facet_grid(.~model) +
 						scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8)) +
 						xlab("Trial") + ylab("Looking time") + theme_bw() +
 						scale_fill_brewer(palette = "Dark2") +
 						scale_colour_discrete(name = "Condition",
 											  labels = c("label","no label"))
-print(label_time.data.plot)
-dev.off()
+ggsave("../Results/General.pdf", plot = single_obj.data.plot,
+	   height = 4, width = 9)
+#print(single_obj.data.plot)
+#dev.off()
 # Model plot for LaF
 #pdf("../Results/LaF_results_LT.pdf", w=8, h=5)
 # TODO
