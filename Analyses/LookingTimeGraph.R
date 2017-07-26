3e58166a9449ce9ad3d9886920b98d8df21720ad
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-07-26 18:54:29 +0100

    Grouped all graph generating files, changed a few aspect settings.

---
+++ Analyses/LookingTimeGraph.R
@@ -0,0 +1,142 @@
+# LIBRARY IMPORTS
+library(ggplot2)
+library(Hmisc)
+
+## Summarizes data.
+## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
+##   data: a data frame.
+##   measurevar: the name of a column that contains the variable to be summariezed
+##   groupvars: a vector containing names of columns that contain grouping variables
+##   na.rm: a boolean that indicates whether to ignore NA's
+##   conf.interval: the percent range of the confidence interval (default is 95%)
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+    library(plyr)
+
+    # New version of length which can handle NA's: if na.rm==T, don't count them
+    length2 <- function (x, na.rm=FALSE) {
+        if (na.rm) sum(!is.na(x))
+        else       length(x)
+    }
+
+    # This does the summary. For each group's data frame, return a vector with
+    # N, mean, and sd
+    datac <- ddply(data, groupvars, .drop=.drop,
+      .fun = function(xx, col) {
+        c(N    = length2(xx[[col]], na.rm=na.rm),
+          mean = mean   (xx[[col]], na.rm=na.rm),
+          sd   = sd     (xx[[col]], na.rm=na.rm)
+        )
+      },
+      measurevar
+    )
+
+    # Rename the "mean" column    
+    datac <- rename(datac, c("mean" = measurevar))
+
+    datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+
+    # Confidence interval multiplier for standard error
+    # Calculate t-statistic for confidence interval: 
+    # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+    ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+    datac$ci <- datac$se * ciMult
+
+    return(datac)
+}
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+# DATA HANDLING
+# Import data, either from Category or SingleObject
+exp <- "SingleObject"
+#exp <- "Category"
+LT.data <- read.csv(paste0("../Results/",exp,"_LT.csv"), head=TRUE)
+# Set all factor variables to factors
+LT.data$subject <- as.factor(LT.data$subject)
+LT.data$explo_overlap <- as.factor(LT.data$explo_overlap)
+# Transform trial number to start at 1
+LT.data$trial <- LT.data$trial + 1
+# Summarising data for mean+CI graph
+LT.data.sum <- summarySE(LT.data, measurevar="looking_time",
+								groupvars=c("labelled","model",
+											"theory","trial"),
+								conf.interval=.89)
+
+# GENERATING GRAPHS
+# Graph from data (not models), mean and error bars (CI)
+LT.data.plot <- ggplot(LT.data.sum, aes(x = trial,
+										y = looking_time,
+										colour = labelled,
+										shape = labelled)) +
+				ggtitle(paste0("Results from the ",exp," experiment")) +
+				facet_grid(model~theory) +
+				scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8)) +
+				scale_y_continuous(limits = c(15,65)) +
+				xlab("Trial") + ylab("Looking time") + theme_bw() +
+				theme(panel.grid.minor.x=element_blank(),
+					  plot.title = element_text(size=20,
+												face="bold",
+												hjust=0.5),
+					  axis.title = element_text(size=14)) +
+				scale_fill_brewer(palette = "Dark2") +
+				scale_shape_manual(name = "Condition",
+								   breaks = c("label","no_label"),
+								   labels = c("label","no label"),
+								   values = c(21,24)) +
+				scale_colour_discrete(name = "Condition",
+									  breaks = c("label","no_label"),
+									  labels = c("label","no label")) +
+				geom_errorbar(aes(ymin=looking_time-ci,
+								  ymax=looking_time+ci),
+							  colour="black", width=.1,
+							  position=position_dodge(0.3)) +
+				geom_line(position=position_dodge(0.3)) +
+				geom_point(position=position_dodge(0.3),
+						   size=1.5, fill="white")
+ggsave(paste0("../Results/Mean+CI_",exp,".pdf"), plot = LT.data.plot,
+	   height = 8, width = 9)
