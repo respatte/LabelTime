eeecfda3c6c0bbefab18123e98a3c665fd6274cc
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-07-27 12:12:49 +0100

    4pps, noise in [.1, .3], l_size=8, h_ratio=.6

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def run_subjects(n_subjects, experiment, bash_i=0, explo_ratio=None,
 	if experiment == "SingleObject":
 		condition = SingleObjectExperiment((8,10,8), (.1,explo_ratio),
 										   n_subjects, n_subjects*bash_i,
-										   pps=3, h_ratio=.6)
+										   pps=4, h_ratio=.6)
 	elif experiment == "Category":
 		condition = CategoryExperiment((8,10,0), (.1, 0),
 									   n_subjects, n_subjects*bash_i,
-									   pps=3, theta_p=(100, 5))
+									   pps=4, theta_p=(100, 5))
 	noise = "np.random.uniform(.1, .3, (m,n)) * "
 	noise += "(2 * np.random.binomial(1, .5, (m,n)) - 1)"
 	reinit = None
