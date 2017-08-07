b03810d3c3a622fdd81cb3ba7782b7e0b231891b
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-08-07 16:41:57 +0100

    Plotting example of category structure

---
+++ Analyses/CategoryStructure.R
@@ -0,0 +1,22 @@
+# LIBRARY IMPORTS
+library(ggplot2)
+library(Hmisc)
+library(plyr)
+
+# DATA HANDLING
+cat <- read.csv("../Results/CategoryStructure.csv", head=TRUE)
+
+# COMPUTE PCA
+cat.pca <- prcomp(cat, retx=T)
+# Replace previous values with PCA values
+cat[,2:11] <- cat.pca$x
+
+# PLOT
+cat.plot <- ggplot(data=cat, aes(x=dim0, y=dim1, colour=factor(category), shape=factor(category))) +
+  xlab("PCA1 - 83.59% of variance explained") +
+  ylab("PCA2 - 5.955% of variance explained") +
+  scale_colour_brewer(palette="Dark2", guide=F) +
+  scale_shape_manual(values=c(16,18), guide=F) +
+  geom_point(size=6)
+ggsave("../Results/CategoryStructure.pdf", plot=cat.plot,
+       width=3, height=3)
