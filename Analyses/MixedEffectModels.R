8a249d935edfcfff5425b714b5807e55548bc6af
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-08-07 01:55:53 +0100

    Plotted random effects (caterpillar plots) for both experiments.

--- Analyses/MixedEffectModels.R
+++ Analyses/MixedEffectModels.R
@@ -1,7 +1,50 @@
 # LIBRARY IMPORTS
 library(ggplot2)
+library(broom)
+library(reshape2)
+library(plyr)
 library(dplyr)
 library(lme4)
+
+##' @importFrom reshape2 melt
+##' @importFrom plyr ldply name_rows 
+augment.ranef.mer <- function(x,
+                              ci.level=0.9,
+                              reorder=TRUE,
+                              order.var=1) {
+  tmpf <- function(z) {
+    if (is.character(order.var) && !order.var %in% names(z)) {
+      order.var <- 1
+      warning("order.var not found, resetting to 1")
+    }
+    ## would use plyr::name_rows, but want levels first
+    zz <- data.frame(level=rownames(z),z,check.names=FALSE)
+    if (reorder) {
+      ## if numeric order var, add 1 to account for level column
+      ov <- if (is.numeric(order.var)) order.var+1 else order.var
+      zz$level <- reorder(zz$level, zz[,order.var+1], FUN=identity)
+    }
+    ## Q-Q values, for each column separately
+    qq <- c(apply(z,2,function(y) {
+      qnorm(ppoints(nrow(z)))[order(order(y))]
+    }))
+    rownames(zz) <- NULL
+    pv   <- attr(z, "postVar")
+    cols <- 1:(dim(pv)[1])
+    se   <- unlist(lapply(cols, function(i) sqrt(pv[i, i, ])))
+    ## n.b.: depends on explicit column-major ordering of se/melt
+    zzz <- cbind(melt(zz,id.vars="level",value.name="estimate"),
+                 qq=qq,std.error=se)
+    ## reorder columns:
+    subset(zzz,select=c(variable, level, estimate, qq, std.error))
+  }
+  dd <- ldply(x,tmpf,.id="grp")
+  ci.val <- -qnorm((1-ci.level)/2)
+  transform(dd,
+            p=2*pnorm(-abs(estimate/std.error)), ## 2-tailed p-val
+            lb=estimate-ci.val*std.error,
+            ub=estimate+ci.val*std.error)
+}
 
 # DATA HANDLING
 # Import data, from both Category and SingleObject
@@ -64,8 +107,9 @@ LT.SingObj.random.comparison <- anova(LT.SingObj.random.0,
                                       LT.SingObj.random.4,
                                       LT.SingObj.random.5)
 #print(LT.SingObj.random.comparison)
-# Selecting final model, computing confidence intervals for parameter estimates
+# Selecting final model
 LT.SingObj.final <- LT.SingObj.random.5
+# Computing confidence intervals for parameter estimates, storing as a dataframe
 LT.SingObj.CI <- cbind(as.data.frame(confint(LT.SingObj.final, parm="beta_", level=0.89))[1:3,],
                        as.data.frame(confint(LT.SingObj.final, parm="beta_", level=0.97))[1:3,]) # Only keep effects without theory
 LT.SingObj.CI$estimate <- fixef(LT.SingObj.final)[1:3]
@@ -97,12 +141,18 @@ LT.SingObj.LaF.comparison <- anova(LT.SingObj.LaF.lmer.3,
 #print(LT.SingObj.LaF.comparison)
 # Select final fixed effects model
 LT.SingObj.LaF.final <- LT.SingObj.LaF.lmer.0
+# Compute confidence intervals
 LT.SingObj.LaF.CI <- cbind(as.data.frame(confint(LT.SingObj.LaF.final, parm="beta_", level=0.89)),
                            as.data.frame(confint(LT.SingObj.LaF.final, parm="beta_", level=0.97)))
 LT.SingObj.LaF.CI$estimate <- fixef(LT.SingObj.LaF.final)
 LT.SingObj.LaF.CI$parameter <- c("Intercept","Trial","Condition (no label)", "Trial * Condition (no label)") # Add meaningful names
 LT.SingObj.LaF.CI$intercept <- c("Intercept","Coefficients","Coefficients","Coefficients")
 LT.SingObj.LaF.CI$theory <- "Labels as Features"
+# Preparing random effects dataframe for caterpillar plots
+rf <- ranef(LT.SingObj.LaF.final, condVar=T)
+LT.SingObj.LaF.ranef <- augment(rf)
+LT.SingObj.LaF.ranef$theory <- "Labels as Features"
+levels(LT.SingObj.LaF.ranef$variable) <- c("Intercept","Trial","Condition (no label)")
 # COMPOUND REPRESENTATIONS
 # All models taken as previous one minus last effect
 LT.SingObj.CR.lmer.0 <- lmer(looking_time ~ 1 + trial + labelled + trial:labelled +
@@ -119,12 +169,18 @@ LT.SingObj.CR.comparison <- anova(LT.SingObj.CR.lmer.3,
 #print(LT.SingObj.CR.comparison)
 # Select final fixed effects model
 LT.SingObj.CR.final <- LT.SingObj.CR.lmer.2
+# Compute confidence intervals
 LT.SingObj.CR.CI <- cbind(as.data.frame(confint(LT.SingObj.CR.final, parm="beta_", level=0.89)),
                           as.data.frame(confint(LT.SingObj.CR.final, parm="beta_", level=0.97)))
 LT.SingObj.CR.CI$estimate <- fixef(LT.SingObj.CR.final)
 LT.SingObj.CR.CI$parameter <- c("Intercept","Trial") # Add meaningful names
 LT.SingObj.CR.CI$intercept <- c("Intercept","Coefficients")
 LT.SingObj.CR.CI$theory <- "Compound Representations"
+# Preparing random effects dataframe for caterpillar plots
+rf <- ranef(LT.SingObj.CR.final, condVar=T)
+LT.SingObj.CR.ranef <- augment(rf)
+LT.SingObj.CR.ranef$theory <- "Compound Representations"
+levels(LT.SingObj.CR.ranef$variable) <- c("Intercept","Trial","Condition (no label)")
 
 # CATEGORY MODEL -- FIXED EFFECTS
 # All models taken as previous one minus last effect
@@ -145,7 +201,7 @@ LT.Cat.comparison <- anova(LT.Cat.lmer.7,
                            LT.Cat.lmer.4,
                            LT.Cat.lmer.1,
                            LT.Cat.lmer.0)
-print(LT.Cat.comparison)
+#print(LT.Cat.comparison)
 # Select final fixed effects model
 LT.Cat.fixed <- LT.Cat.lmer.0
 
@@ -196,12 +252,18 @@ LT.Cat.LaF.comparison <- anova(LT.Cat.LaF.lmer.3,
 #print(LT.Cat.LaF.comparison)
 # Select final fixed effects model
 LT.Cat.LaF.final <- LT.Cat.LaF.lmer.0
+# Compute confidence intervals
 LT.Cat.LaF.CI <- cbind(as.data.frame(confint(LT.Cat.LaF.final, parm="beta_", level=0.89)),
                        as.data.frame(confint(LT.Cat.LaF.final, parm="beta_", level=0.97)))
 LT.Cat.LaF.CI$estimate <- fixef(LT.Cat.LaF.final)
 LT.Cat.LaF.CI$parameter <- c("Intercept","Trial","Condition (no label)", "Trial * Condition (no label)") # Add meaningful names
 LT.Cat.LaF.CI$intercept <- c("Intercept","Coefficients","Coefficients","Coefficients")
 LT.Cat.LaF.CI$theory <- "Labels as Features"
+# Preparing random effects dataframe for caterpillar plots
+rf <- ranef(LT.Cat.LaF.final, condVar=T)
+LT.Cat.LaF.ranef <- augment(rf)
+LT.Cat.LaF.ranef$theory <- "Labels as Features"
+levels(LT.Cat.LaF.ranef$variable) <- c("Intercept","Trial","Condition (no label)")
 # COMPOUND REPRESENTATIONS
 # All models taken as previous one minus last effect
 LT.Cat.CR.lmer.0 <- lmer(looking_time ~ 1 + trial + labelled + trial:labelled +
@@ -218,12 +280,19 @@ LT.Cat.CR.comparison <- anova(LT.Cat.CR.lmer.3,
 #print(LT.Cat.CR.comparison)
 # Select final fixed effects model
 LT.Cat.CR.final <- LT.Cat.CR.lmer.2
+# Compute confidence intervals
 LT.Cat.CR.CI <- cbind(as.data.frame(confint(LT.Cat.CR.final, parm="beta_", level=0.89)),
                       as.data.frame(confint(LT.Cat.CR.final, parm="beta_", level=0.97)))
 LT.Cat.CR.CI$estimate <- fixef(LT.Cat.CR.final)
 LT.Cat.CR.CI$parameter <- c("Intercept","Trial") # Add meaningful names
 LT.Cat.CR.CI$intercept <- c("Intercept","Coefficients")
 LT.Cat.CR.CI$theory <- "Compound Representations"
+# Preparing random effects dataframe for caterpillar plots
+rf <- ranef(LT.Cat.CR.final, condVar=T)
+LT.Cat.CR.ranef <- augment(rf)
+LT.Cat.CR.ranef$theory <- "Compound Representations"
+levels(LT.Cat.CR.ranef$variable) <- c("Intercept","Trial","Condition (no label)")
+
 
 # MIXED EFFECT MODELS GRAPHS
 
@@ -278,3 +347,21 @@ LT.Cat.plot.CI <- ggplot(data=LT.Cat.all.CI,
              size=1, fill="white")
 ggsave("../Results/FixedEffects_Category.pdf", plot = LT.Cat.plot.CI,
        height = 2, width = 10)
+
+# SINGLE OBJECT -- RANDOM EFFECTS
+LT.SingObj.ranef <- rbind(LT.SingObj.LaF.ranef, LT.SingObj.CR.ranef)
+LT.SingObj.plot.ranef <- ggplot(LT.SingObj.ranef, aes(estimate, level, xmin=lb, xmax=ub)) +
+  xlab("Estimate") + ylab("") + theme_bw() +
+  geom_errorbarh(height=0) + geom_vline(xintercept=0, lty=2) + geom_point() +
+  facet_grid(theory~variable, scale="free")
+ggsave("../Results/RandomEffects_SingleObject.pdf", plot = LT.SingObj.plot.ranef,
+       height = 4, width = 6)
+
+# CATEGORY -- RANDOM EFFECTS
+LT.Cat.ranef <- rbind(LT.Cat.LaF.ranef, LT.Cat.CR.ranef)
+LT.Cat.plot.ranef <- ggplot(LT.Cat.ranef, aes(estimate, level, xmin=lb, xmax=ub)) +
+  xlab("Estimate") + ylab("") + theme_bw() +
+  geom_errorbarh(height=0) + geom_vline(xintercept=0, lty=2) + geom_point() +
+  facet_grid(theory~variable, scale="free")
+ggsave("../Results/RandomEffects_Category.pdf", plot = LT.Cat.plot.ranef,
+       height = 4, width = 6)
