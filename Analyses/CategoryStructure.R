c3cef7223ca232a9d503d1a2dbab81f389e83a7b
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-10-27 14:53:49 +0100

    Changed CategoryStructure to differentiate category prototypes

--- Analyses/CategoryStructure.R
+++ Analyses/CategoryStructure.R
@@ -5,19 +5,21 @@ library(plyr)
 
 # DATA HANDLING
 cat <- read.csv("../Results/CategoryStructure.csv", head=TRUE)
+cat$category <- factor(cat$category)
+cat$cat_phase <- factor(paste(cat$phase,cat$category))
 
 # COMPUTE PCA
-cat.pca <- prcomp(cat, retx=T)
+cat.pca <- prcomp(cat[,3:12], retx=T)
 # Replace previous values with PCA values
-cat[,2:11] <- cat.pca$x
+cat[,3:12] <- cat.pca$x
 
 # PLOT
-cat.plot <- ggplot(data=cat, aes(x=dim0, y=dim1, colour=factor(category), shape=factor(category))) +
+cat.plot <- ggplot(data=cat, aes(x=dim0, y=dim1, colour=category, shape=cat_phase)) +
   xlab("PCA1 - 83.59% of variance explained") +
   ylab("PCA2 - 5.955% of variance explained") +
   theme_bw(base_size = 10) +
   scale_colour_brewer(palette="Dark2", guide=F) +
-  scale_shape_manual(values=c(16,18), guide=F) +
+  scale_shape_manual(values=c(21,23,16,18), guide=F) +
   geom_point(size=6)
 ggsave("../Results/CategoryStructure.pdf", plot=cat.plot,
        width=3.5, height=3)
