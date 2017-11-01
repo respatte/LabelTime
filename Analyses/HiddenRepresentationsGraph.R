ccac94a6603458ba31190a91c4fe6b554efb9535
Arthur Capelier-Mourguy <a.capelier-mourguy@lancaster.ac.uk>
2017-11-01 15:49:00 +0000

    Ignoring STM hidden representations

--- Analyses/HiddenRepresentationsGraph.R
+++ Analyses/HiddenRepresentationsGraph.R
@@ -55,70 +55,62 @@ if (!compute_distances) {
 } else {
   # DATA HANDLING
   # Import data, from both Category and SingleObject
-  h_rep.LTM <- read.csv("../Results/Category_hidden_LTM.csv", head=TRUE)
-  h_rep.STM <- read.csv("../Results/Category_hidden_STM.csv", head=TRUE)
-  # Add memory_type column on the left
-  h_rep.LTM <- cbind(memory_type="LTM", h_rep.LTM)
-  h_rep.STM <- cbind(memory_type="STM", h_rep.STM)
-  # Binding dataframes together
-  h_rep <- rbind.fill(h_rep.LTM,h_rep.STM)
+  h_rep <- read.csv("../Results/Category_hidden_LTM.csv", head=TRUE)
   # Initialise d to maximum possible number of cases
-  # (subject(80)*step(24)*memory_type(LTM;STM)*dist_type(labelled;unlabelled;between;r_labelled;r_unlabelled) = 19200)
-  d <- data.frame(memory_type=factor(c("LTM", "STM")), subject=numeric(19200), theory=factor(c("CR", "LaF")), step=0,
+  # (subject(80)*step(20)*dist_type(labelled;unlabelled;between;r_labelled;r_unlabelled) = 19200)
+  d <- data.frame(subject=numeric(8000), theory=factor(c("CR", "LaF_ignore_missing","LaF_treat_missing")), step=0,
                   dist_type=NA, mu=0)
   # Loop over subject, memory_type, and step
   i <- 1
   for (subject in 0:79) {
     print(subject)
-    for (memory_type in levels(h_rep$memory_type)){
-      for (step in levels(factor(h_rep$step[h_rep$subject==subject & h_rep$memory_type==memory_type]))){
-        index <- h_rep$subject==subject & h_rep$step==step & h_rep$memory_type==memory_type
-        # WITHIN CATEGORY MEAN DISTANCE
-        dist.l <- c()  # Labelled category (coded 0 in df)
-        dist.nl <- c() # Unlabelled category (coded 1 in df)
-        # Create index for category
-        index.l <- index & h_rep$labelled=="label"
-        index.nl <- index & h_rep$labelled=="no_label"
-        for (e1 in 0:2){
-          # Create index for first exemplar
-          index1.l <- index.l & h_rep$exemplar==e1
-          index1.nl <- index.nl & h_rep$exemplar==e1+4
-          for (e2 in (e1+1):3){
-            # Create index for second exemplar
-            index2.l <- index.l & h_rep$exemplar==e2
-            index2.nl <- index.nl & h_rep$exemplar==e2+4
-            # Append distances
-            dist.l <- c(dist.l,
-                        dist(rbind(h_rep[index1.l, -(1:6)],
-                                   h_rep[index2.l, -(1:6)]))[1])
-            #print(c("Added distance between",e1,"and",e2))
-            dist.nl <- c(dist.nl,
-                         dist(rbind(h_rep[index1.nl, -(1:6)],
-                                    h_rep[index2.nl, -(1:6)]))[1])
-            #print(c("Added distance between",e1+4,"and",e2+4))
-          }
+    for (step in levels(factor(h_rep$step[h_rep$subject==subject]))){
+      index <- h_rep$subject==subject & h_rep$step==step
+      # WITHIN CATEGORY MEAN DISTANCE
+      dist.l <- c()  # Labelled category (coded 0 in df)
+      dist.nl <- c() # Unlabelled category (coded 1 in df)
+      # Create index for category
+      index.l <- index & h_rep$labelled=="label"
+      index.nl <- index & h_rep$labelled=="no_label"
+      for (e1 in 0:2){
+        # Create index for first exemplar
+        index1.l <- index.l & h_rep$exemplar==e1
+        index1.nl <- index.nl & h_rep$exemplar==e1+4
+        for (e2 in (e1+1):3){
+          # Create index for second exemplar
+          index2.l <- index.l & h_rep$exemplar==e2
+          index2.nl <- index.nl & h_rep$exemplar==e2+4
+          # Append distances
+          dist.l <- c(dist.l,
+                      dist(rbind(h_rep[index1.l, -(1:5)],
+                                 h_rep[index2.l, -(1:5)]))[1])
+          #print(c("Added distance between",e1,"and",e2))
+          dist.nl <- c(dist.nl,
+                       dist(rbind(h_rep[index1.nl, -(1:5)],
+                                  h_rep[index2.nl, -(1:5)]))[1])
+          #print(c("Added distance between",e1+4,"and",e2+4))
         }
-        # Save mean and sd of distance for labelled category with all info
-        d[i,] <- c(h_rep[index1.l, 1:4], "labelled",  mean(dist.l))
-        #print("Added a row for dist.l")
-        # Save mean and sd of distance for unlabelled category with all info
-        d[i + 1,] <- c(h_rep[index1.nl, 1:4], "unlabelled", mean(dist.nl))
-        #print("Added a row for dist.nl")
-        # PROTOTYPE: NOT WORKING FOR NOW (mean(...) returns warning because of non-numerical values leading to NA)
-        # Compute centre ("prototype") of each category
-        proto.l <- colMeans(h_rep[index.l, -(1:6)])
-        #print("Computed labelled prototype")
-        proto.nl <- colMeans(h_rep[index.nl, -(1:6)])
-        #print("Computed unlabelled prototype")
-        # Save distances between categories (sd=0 here)
-        d[i + 2,] <- c(h_rep[index1.nl, 1:4], "between",
-                       dist(rbind(proto.l, proto.nl))[1])
-        #print("Added a row for LTM between- distance")
-        # Relative distances
-        d[i + 3,] <- c(h_rep[index1.l, 1:4], "r_labelled",  d[i,6]/d[i+2,6])
-        d[i + 4,] <- c(h_rep[index1.nl, 1:4], "r_unlabelled",  d[i+1,6]/d[i+2,6])
-        i <- i + 5
       }
+      # Save mean and sd of distance for labelled category with all info
+      d[i,] <- c(h_rep[index1.l, 1:3], "labelled",  mean(dist.l))
+      #print("Added a row for dist.l")
+      # Save mean and sd of distance for unlabelled category with all info
+      d[i + 1,] <- c(h_rep[index1.nl, 1:3], "unlabelled", mean(dist.nl))
+      #print("Added a row for dist.nl")
+      # PROTOTYPE: NOT WORKING FOR NOW (mean(...) returns warning because of non-numerical values leading to NA)
+      # Compute centre ("prototype") of each category
+      proto.l <- colMeans(h_rep[index.l, -(1:5)])
+      #print("Computed labelled prototype")
+      proto.nl <- colMeans(h_rep[index.nl, -(1:5)])
+      #print("Computed unlabelled prototype")
+      # Save distances between categories (sd=0 here)
+      d[i + 2,] <- c(h_rep[index1.nl, 1:3], "between",
+                     dist(rbind(proto.l, proto.nl))[1])
+      #print("Added a row for LTM between- distance")
+      # Relative distances
+      d[i + 3,] <- c(h_rep[index1.l, 1:3], "r_labelled",  d[i,6]/d[i+2,6])
+      d[i + 4,] <- c(h_rep[index1.nl, 1:3], "r_unlabelled",  d[i+1,6]/d[i+2,6])
+      i <- i + 5
     }
   }
   # Code dist_type as a factor, get rid of NAs
@@ -129,18 +121,15 @@ if (!compute_distances) {
 }
 
 # DATA HANDLING
-# Get rid of STM
-d <- d[d$memory_type=="LTM",2:6]
 # Create two distance type columns (labeled/unlabeled/between, and absolute/relative/between)
 d$abs_rel <- recode(d$dist_type, "'r_labelled'='Relative - Within'; 'r_unlabelled'='Relative - Within'; 'labelled'='Absolute - Within'; 'unlabelled'='Absolute - Within'; 'between'='Between'")
 d$dist_type <- recode(d$dist_type, "'r_labelled'='Labelled'; 'r_unlabelled'='Unlabelled'; 'labelled'='Labelled'; 'unlabelled'='Unlabelled'; 'between'='Between'")
 
 # GRAPH
-# Absolute distance
+# Select observations for plot, dropping unusued factors
+d <- droplevels(d[d$step>0 & d$step <= 2000,])
 # Get summary of the data
 d.sum <- summarySE(d, measurevar="mu", groupvars=c("theory", "step", "dist_type", "abs_rel"), conf.interval=.95)
-# Select observations for plot, dropping unusued factors
-d.sum <- droplevels(d.sum[d.sum$step>0,])
 # Create plot
 d.plot <- ggplot(d.sum,
                  aes(x = step,
@@ -157,4 +146,4 @@ d.plot <- ggplot(d.sum,
   geom_line(size=.1) +
   geom_ribbon(aes(ymin=mu-ci, ymax=mu+ci, fill=dist_type), alpha=0.1, size=0)
 # Save plot
-ggsave("../Results/Distances.png", plot = d.plot, height = 5, width = 3.1)
+ggsave("../Results/Distances.png", plot = d.plot, height = 5, width = 6.5)
